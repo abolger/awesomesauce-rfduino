a39764fa4ea2f0f9fa8aa450232961fb5c473fed
abolger <adrienne.bolger@gmail.com>
2013-11-09 20:53:01 -0500

    Committing addition of constants that represent the agreed upon Bluetooth Low Energy standard labels for different types of services and data (e.g. walking speed, datetime, etc.). Constant values come from bluetooth.org

---
+++ RFDuino-AndroidExamples/src/org/bluetooth/Characteristic.java
@@ -0,0 +1,97 @@
+package org.bluetooth;
+
+/*** Constants containing human readable translations of BLE characteristics. Constants derived from
+ * Bluetooth SIG adopted standard here
+ * 
+ * https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicsHome.aspx
+ * @author adrienne
+ *
+ */
+
+
+public class Characteristic {
+
+public final int  alert_category_id	= 0x2A43;	
+public final int  alert_category_id_bit_mask	= 0x2A42;	
+public final int  alert_level	= 0x2A06;	
+public final int  alert_notification_control_point	= 0x2A44;	
+public final int  alert_status	= 0x2A3F;	
+public final int  gap_appearance	= 0x2A01;	
+public final int  battery_level	= 0x2A19;	
+public final int  blood_pressure_feature	= 0x2A49;	
+public final int  blood_pressure_measurement	= 0x2A35;	
+public final int  body_sensor_location	= 0x2A38;	
+public final int  boot_keyboard_input_report	= 0x2A22;	
+public final int  boot_keyboard_output_report	= 0x2A32;	
+public final int  boot_mouse_input_report	= 0x2A33;	
+public final int  csc_feature	= 0x2A5C;	
+public final int  csc_measurement	= 0x2A5B;	
+public final int  current_time	= 0x2A2B;	
+public final int cycling_power_control_point	= 0x2A66;	
+public final int cycling_power_feature	= 0x2A65;	
+public final int cycling_power_measurement	= 0x2A63;	
+public final int  cycling_power_vector	= 0x2A64;	
+public final int  date_time	= 0x2A08;	
+public final int  day_date_time	= 0x2A0A;	
+public final int  day_of_week	= 0x2A09;	
+public final int  gap_device_name	= 0x2A00;	
+public final int  dst_offset	= 0x2A0D;	
+public final int  exact_time_256	= 0x2A0C;	
+public final int  firmware_revision_string	= 0x2A26;	
+public final int  glucose_feature	= 0x2A51;	
+public final int  glucose_measurement	= 0x2A18;	
+public final int  glucose_measurement_context	= 0x2A34;	
+public final int  hardware_revision_string	= 0x2A27;	
+public final int  heart_rate_control_point	= 0x2A39;	
+public final int  heart_rate_measurement	= 0x2A37;	
+public final int  hid_control_point	= 0x2A4C;	
+public final int  hid_information	= 0x2A4A;	
+public final int  ieee_11073_20601_regulatory_certification_data_list	= 0x2A2A;	
+public final int  intermediate_blood_pressure	= 0x2A36;	
+public final int  intermediate_temperature	= 0x2A1E;	
+public final int  ln_control_point	= 0x2A6B;	
+public final int  ln_feature	= 0x2A6A;	
+public final int  local_time_information	= 0x2A0F;	
+public final int  location_and_speed	= 0x2A67;	
+public final int  manufacturer_name_string	= 0x2A29;	
+public final int  measurement_interval	= 0x2A21;	
+public final int  model_number_string	= 0x2A24;	
+public final int  navigation	= 0x2A68;	
+public final int  new_alert	= 0x2A46;	
+public final int  gap_peripheral_preferred_connection_parameters	= 0x2A04;	
+public final int  gap_peripheral_privacy_flag	= 0x2A02;	
+public final int  pnp_id	= 0x2A50;	
+public final int  position_quality	= 0x2A69;	
+public final int  protocol_mode	= 0x2A4E;	
+public final int  gap_reconnection_address	= 0x2A03;	
+public final int  record_access_control_point	= 0x2A52;	
+public final int  reference_time_information	= 0x2A14;	
+public final int  report	= 0x2A4D;	
+public final int  report_map	= 0x2A4B;	
+public final int  ringer_control_point	= 0x2A40;	
+public final int  ringer_setting	= 0x2A41;	
+public final int  rsc_feature	= 0x2A54;	
+public final int  rsc_measurement	= 0x2A53;	
+public final int  sc_control_point	= 0x2A55;	
+public final int  scan_interval_window	= 0x2A4F;	
+public final int  scan_refresh	= 0x2A31;	
+public final int  sensor_location	= 0x2A5D;	
+public final int  serial_number_string	= 0x2A25;	
+public final int  gatt_service_changed	= 0x2A05;	
+public final int  software_revision_string	= 0x2A28;	
+public final int  supported_new_alert_category	= 0x2A47;	
+public final int  supported_unread_alert_category	= 0x2A48;	
+public final int  system_id	= 0x2A23;	
+public final int  temperature_measurement	= 0x2A1C;	
+public final int  temperature_type	= 0x2A1D;	
+public final int  time_accuracy	= 0x2A12;	
+public final int  time_source	= 0x2A13;	
+public final int  time_update_control_point	= 0x2A16;	
+public final int  time_update_state	= 0x2A17;	
+public final int  time_with_dst	= 0x2A11;	
+public final int  time_zone	= 0x2A0E;	
+public final int  tx_power_level	= 0x2A07;	
+public final int  unread_alert_status	= 0x2A45;
+public final int wer	= 0x1804;	
+
+}
---
+++ RFDuino-AndroidExamples/src/org/bluetooth/Service.java
@@ -0,0 +1,42 @@
+package org.bluetooth;
+
+/** This is a class of constants defined for easy reference from the Bluetooth Low Energy Developer's specification
+ * on GATT Service and Characteristics.
+ * https://developer.bluetooth.org/gatt/Pages/GATT-Specification-Documents.aspx
+ * @author adrienne
+ *
+ */
+
+public class Service {
+	
+public final int  alert_notification	= 0x1811	;  
+public final int  battery_service	= 0x180F	;  
+public final int  blood_pressure	= 0x1810	;  
+public final int  current_time	= 0x1805	;  
+public final int  cycling_power	= 0x1818	;  
+public final int  cycling_speed_and_cadence	= 0x1816	;  
+public final int  device_information	= 0x180A	;  
+public final int  generic_access	= 0x1800	;  
+public final int  generic_attribute	= 0x1801	;  
+public final int  glucose	= 0x1808	;  
+public final int  health_thermometer	= 0x1809	;  
+public final int  heart_rate	= 0x180D	;  
+public final int  human_interface_device	= 0x1812	;  
+public final int  immediate_alert	= 0x1802	;  
+public final int  link_loss	= 0x1803	;  
+public final int  location_and_navigation	= 0x1819	;  
+public final int  next_dst_change	= 0x1807	;  
+public final int  phone_alert_status	= 0x180E	;  
+public final int  reference_time_update	= 0x1806	;  
+public final int  running_speed_and_cadence	= 0x1814	;  
+public final int  scan_parameters	= 0x1813	;  
+public final int  tx_power	= 0x1804;
+
+
+//Characteristics:
+
+
+
+
+
+}
